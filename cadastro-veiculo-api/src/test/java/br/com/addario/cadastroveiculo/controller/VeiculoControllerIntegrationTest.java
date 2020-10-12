ba421904c6d3e8a1b2a79ca351c4bbf5b2d73e2c
José Addario <j.addario@gmail.com>
2020-10-12 13:06:44 -0300

    teste para not found

--- cadastro-veiculo-api/src/test/java/br/com/addario/cadastroveiculo/controller/VeiculoControllerIntegrationTest.java
+++ cadastro-veiculo-api/src/test/java/br/com/addario/cadastroveiculo/controller/VeiculoControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package br.com.addario.cadastroveiculo.controller;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,48 +33,47 @@ class VeiculoControllerIntegrationTest {
 
 	@Autowired
 	private MockMvc mvc;
-	
+
 	@MockBean
 	private VeiculoService service;
 
 	private static Veiculo veiculo;
 
 	private static List<Veiculo> veiculos;
-	
+
 	@BeforeAll
 	static void init() {
-		veiculo = new Veiculo();
-		veiculo.setId(1L);
-		veiculo.setModelo("Monza");
-		veiculo.setMarca("Fiat");
-		veiculo.setDescricao("Monza Preto");
-		veiculo.setAno(1995);
+		veiculo = new Veiculo(1L, "Monza", "Fiat", 1995, "Monza Preto");
 		veiculos = Arrays.asList(veiculo);
 	}
-	
+
 	@Test
 	void testaConsultarTodosOsVeiculos() throws Exception {
 		given(service.getTodosOsVeiculos()).willReturn(veiculos);
 		mvc.perform(get("/veiculos")).andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().json("[{'id': 1,'modelo': 'Monza','marca': 'Fiat', 'descricao':'Monza Preto', 'ano': 1995, 'vendido': false}]"));
+				.andExpect(status().isOk()).andExpect(content().json(
+						"[{'id': 1,'modelo': 'Monza','marca': 'Fiat', 'descricao':'Monza Preto', 'ano': 1995, 'vendido': false}]"));
 	}
-	
-	
+
 	@Test
 	void testaCadastrarUmVeiculo() throws Exception {
 		mvc.perform(post("/veiculos")
-			.content(asJsonString(new Veiculo(2l, "Chevete", "Chevrolet", 1992, "Chevrolet Branco Duas portas")))
-		    .contentType("application/json"))
-			.andExpect(status().isOk());
+				.content(asJsonString(new Veiculo(2l, "Chevete", "Chevrolet", 1992, "Chevrolet Branco Duas portas")))
+				.contentType("application/json")).andExpect(status().isOk());
 
 	}
-	
+
+	@Test
+	void testaRemoverVeiculo() throws Exception {
+		mvc.perform(delete("/veiculos/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
+
+
 	public static String asJsonString(final Object obj) {
-	    try {
-	        return new ObjectMapper().writeValueAsString(obj);
-	    } catch (Exception e) {
-	        throw new RuntimeException(e);
-	    }
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
